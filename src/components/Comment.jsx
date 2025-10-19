469eccf3ab9a7a506840730168012f6f9115f162
Aflah Paruthicode <aflutech2@gmail.com>
2025-10-19 22:17:08 +0530

    Comment Design And Watch Video Section Back Version

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,15 +3,15 @@ import React from "react";
 const Comment = ({ data }) => {
   const { name, text, replies } = data;
   return (
-    <div className="flex">
+    <div className="flex gap-2">
       <img className="w-12 h-12"
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtRs_rWILOMx5-v3aXwJu7LWUhnPceiKvvDg&s"
         alt="user"
       />
       <div className="px-3">
-        <p className="font-bold">{name}</p>
+        <p className="font-semibold text-[14px]">@{name}</p>
         <p>{text}</p>
-      </div>
+      </div> 
     </div> 
   );
 };
