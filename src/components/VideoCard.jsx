d19f21072c05910b114ccace149cfe7526637624
Aflah Paruthicode <aflutech2@gmail.com>
2025-10-25 12:51:07 +0530

    Videos Suggestions done

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,32 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { formatViews } from "../utils/formateCount";
+import { timeAgo } from "../utils/timeAgo";
 
- const VideoCard = ({ info }) => {
+ const VideoCard = ({ info,from=false }) => {
   if (!info) return;
   const { snippet, statistics } = info;
-  const { channelTitle, title, thumbnails } = snippet;
+  const { channelTitle, title, thumbnails,publishedAt } = snippet;
 
   return (
     <Link
-      to={"watch?v=" + info.id}
-      className="p-2 w-1/4 z-10 hover:bg-gray-200 rounded-2xl transition-colors delay-150 duration-500"
+      to={from ? "?v=" + info.id : "watch?v=" + info.id}
+      className={`p-2  z-10 hover:bg-gray-200 rounded-2xl transition-colors delay-150 duration-500 ${from ? 'w-full p-0' : 'w-1/4'}`}
     >
-      <div className="">
+      <div className={`${from ? 'flex' : ''}`}>
         <img
-          className="rounded-lg aspect-video object-cover z-10"
+          className={`rounded-lg aspect-video object-cover z-10 ${from ? 'w-40 h-24 mx-2' : ''}`}
           src={thumbnails.high.url}
           alt=""
         />
         <ul>
-          <li className="font-semibold">
-            {title.length > 85 ? title.slice(0, 82) + "..." : title}
+          <li className={`font-semibold ${from ? 'text-[14px]' : ''}`}>
+            {from ? title.slice(0,50) + '...' : title.length > 85 ? title.slice(0, 82) + "..." : title}
           </li>
           <li className="text-gray-600 font-normal text-[14px]">
             {channelTitle}
           </li>
           <li className="text-gray-600 font-normal text-[14px]">
-            {formatViews(statistics.viewCount)} views
+            {formatViews(statistics.viewCount)} views  {timeAgo(publishedAt)}
           </li>
         </ul>
       </div>
--- src/components/WatchVideo.jsx
+++ src/components/WatchVideo.jsx
@@ -6,14 +6,16 @@ import CommentsContainer from "./CommentsContainer";
 import LiveChat from "./LiveChat";
 import { ChannelDetails } from "./ChannelDetails";
 import { AdsLogo } from "../utils/constants";
-
+import VideoCard from "./VideoCard";
 
 const WatchVideo = () => {
   const [searchparams] = useSearchParams();
-  const allVideos = useSelector((store) => store.videos.videos)
-  console.log('all : ',allVideos)
-  const videoDetails = allVideos.find(item => item.id == searchparams.get("v"))
-  console.log('video here : ',videoDetails)
+  const allVideos = useSelector((store) => store.videos.videos);
+  console.log("all : ", allVideos);
+  const videoDetails = allVideos.find(
+    (item) => item.id == searchparams.get("v")
+  );
+  console.log("video here : ", videoDetails);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(closeMenu());
@@ -21,7 +23,7 @@ const WatchVideo = () => {
 
   return (
     <div className="mt-[80px] px-12 z-10 w-screen">
-      <div className="flex gap-8"> 
+      <div className="flex gap-8">
         <iframe
           className="rounded-xl"
           width="1380"
@@ -40,31 +42,57 @@ const WatchVideo = () => {
       </div>
       <div className="flex w-full justify-between">
         <div className="w-[1380px]">
-
-        <ChannelDetails details={videoDetails.snippet} counts={videoDetails.statistics} /> 
+          <ChannelDetails
+            details={videoDetails.snippet}
+            counts={videoDetails.statistics}
+          />
         </div>
-        <div className="h-40 border cursor-pointer rounded-2xl z-20 mt-5 border-gray-200" onClick={() => window.location.replace('https://www.almircollections.com')}>
+        <div
+          className="h-40 border cursor-pointer rounded-2xl z-20 mt-5 border-gray-200"
+          onClick={() =>
+            window.location.replace("https://www.almircollections.com")
+          }
+        >
           <img
             className="w-[400px] h-20 object-cover rounded-t-2xl"
             src="/adWatchPage.png"
             alt=""
-          /> 
-          <div className="p-3 flex w-full items-center justify-between"> 
-            <img className="w-10 border border-gray-200 rounded-full " src={AdsLogo} alt="" /> 
-            <p>Almir Collections <br /> <span className="text-sm font-bold">Sponsored  <span className="font-normal">almircollections.com</span></span></p>
-            <button className="bg-blue-700 text-white font-semibold py-2 px-4 rounded-full">Visit site</button>
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#00000"><path d="M480-361q-8 0-15-2.5t-13-8.5L268-556q-11-11-11-28t11-28q11-11 28-11t28 11l156 156 156-156q11-11 28-11t28 11q11 11 11 28t-11 28L508-372q-6 6-13 8.5t-15 2.5Z"/></svg>
+          />
+          <div className="p-3 flex w-full items-center justify-between">
+            <img
+              className="w-10 border border-gray-200 rounded-full "
+              src={AdsLogo}
+              alt=""
+            />
+            <p>
+              Almir Collections <br />{" "}
+              <span className="text-sm font-bold">
+                Sponsored{" "}
+                <span className="font-normal">almircollections.com</span>
+              </span>
+            </p>
+            <button className="bg-blue-700 text-white font-semibold py-2 px-4 rounded-full">
+              Visit site
+            </button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#00000"
+            >
+              <path d="M480-361q-8 0-15-2.5t-13-8.5L268-556q-11-11-11-28t11-28q11-11 28-11t28 11l156 156 156-156q11-11 28-11t28 11q11 11 11 28t-11 28L508-372q-6 6-13 8.5t-15 2.5Z" />
+            </svg>
           </div>
         </div>
       </div>
       <div className="flex w-full justify-between">
-      <CommentsContainer />
-      {
-        allVideos.map((video) => {
-
-          <VideoCard key={video.id} info={video} />
-        })
-      }
+        <CommentsContainer />
+        <div className="w-[400px]">
+          {allVideos.map((video,ind) => (
+            <VideoCard key={ind} info={video} from={'watchPage'} />
+          ))}
+        </div> 
       </div>
     </div>
   );
