9f3b7e59d11da84e3a784279f74d4ea0d5dc8284
Aflah Paruthicode <aflutech2@gmail.com>
2025-10-28 12:23:29 +0530

    Error Page  Done

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Error = () => {
   return (
-    <div>Error</div>
-  )
-}
+    <div className="w-full h-screen bg-[#f1f1f1] flex justify-center items-center flex-col">
+      <img src="/404.png" alt="" />
+      <p className="text-center py-3 leading-5">
+        This page isn't available. Sorry about that. <br /> 
+        Try searching for something else. 
+      </p>  
+      <Link className="font-bold border border-white p-2 rounded-lg flex items-center gap-2" to={'/'}> <span className="bg-red-500 text-white p-1 rounded-lg">Go</span>  To Home</Link>
+    </div>
+  );
+};
 
-export default Error
+export default Error;
